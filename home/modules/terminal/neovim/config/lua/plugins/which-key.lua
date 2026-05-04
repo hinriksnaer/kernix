dda6abe144ea69409191eb66dc876a3301518fb3
hinriksnaer <hgudmund@redhat.com>
2026-05-04 17:04:53 -0400

    Move session bindings from <leader>S to <leader>P (Persistence)
    Avoids home row mod Shift bleed between <leader>ss (Search Symbols)
    and <leader>Ss (Session restore) which caused accidental session
    restores appearing as new tabs.

--- home/modules/terminal/neovim/config/lua/plugins/which-key.lua
+++ home/modules/terminal/neovim/config/lua/plugins/which-key.lua
@@ -53,7 +53,7 @@ return {
       { '<leader>w', group = '󰖯 Window' }, -- Window management (smart-splits Ctrl+hjkl, Alt+hjkl reserved for tmux)
       { '<leader>q', group = ' Quickfix' }, -- Quickfix & location list
       { '<leader>s', group = ' Search' }, -- Buffer/file-scoped search (fzf-lua)
-      { '<leader>S', group = '󰁯 Session' }, -- Session management
+      { '<leader>P', group = '󰁯 Persistence' }, -- Session management
       { '<leader>h', group = '󰛢 Harpoon' }, -- Harpoon file marks
       { '<leader>d', group = ' Debug' }, -- DAP debugging
       { '<leader>\\', group = ' Toggle' }, -- mini.basics option toggles
